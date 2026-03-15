9ded591d0d90ba9778eacb16e84ccd8c86dcd8f6
NguyenVuong1511 <nguyenvuong15112005@gmail.com>
2026-03-15 22:41:11 +0700

    Firt commit

---
+++ app/BTL_Mobile/app/src/main/java/com/example/mathkid/Welcome.java
@@ -0,0 +1,78 @@
+package com.example.mathkid;
+
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Lấy Icon từ Layout
+        ImageView iconStar = findViewById(R.id.icon_star);
+        ImageView iconPencil = findViewById(R.id.icon_pencil);
+        ImageView iconBallon = findViewById(R.id.icon_ballon);
+        ImageView iconBook = findViewById(R.id.icon_book);
+        ImageView iconPaint = findViewById(R.id.icon_paint);
+        // Load Animation Floating (Hoạt ảnh bay bồng bềnh)
+        Animation animationFloating = AnimationUtils.loadAnimation(this, R.anim.floating);
+
+        // Bắt đầu chạy hoạt ảnh cho pencil
+        iconPencil.startAnimation(animationFloating);
+        iconBook.startAnimation(animationFloating);
+        iconBallon.startAnimation(animationFloating);
+        iconPaint.startAnimation(animationFloating);
+
+
+        // Tạo animation xoay quanh vòng tròn cho icon_star
+        ValueAnimator orbitAnimator = ValueAnimator.ofFloat(0f, 360f);
+        orbitAnimator.setDuration(10000); // 10 giây cho một vòng
+        orbitAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        orbitAnimator.setInterpolator(new LinearInterpolator());
+        orbitAnimator.addUpdateListener(animation -> {
+            float angle = (float) animation.getAnimatedValue();
+            ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) iconStar.getLayoutParams();
+            layoutParams.circleAngle = angle;
+            iconStar.setLayoutParams(layoutParams);
+        });
+        orbitAnimator.start();
+
+        TextView title = findViewById(R.id.titleWelcome);
+
+        SpannableString text = new SpannableString("WELCOME TO MATHKIDS!");
+
+        text.setSpan(new ForegroundColorSpan(Color.parseColor("#4CAF50")),
+                0, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        text.setSpan(new ForegroundColorSpan(Color.parseColor("#FF9800")),
+                11, text.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        title.setText(text);
+    }
+}
